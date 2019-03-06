814140711d3033543958b51bee67a62cae489998
Terone Madhanayaka <34730459+bterone@users.noreply.github.com>
2019-03-06 15:35:17 +0530

    Created new user form and calls different routes depending on user type

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,19 @@ class UsersController < ApplicationController
     def new
 
     end
+
+    def create
+        case route_to params
+        when :outsider
+            render plain: params[:user].inspect
+        when :student
+            render plain: params[:user].inspect
+        when :instructor
+            render 'login'
+        end
+    end
+
+    private def route_to params
+        params[:route_to].keys.first.to_sym
+    end
 end

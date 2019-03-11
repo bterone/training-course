6fabb5e558bdb3c12d9cd5af2a4a51ae30aad8cc
Terone Madhanayaka <34730459+bterone@users.noreply.github.com>
2019-03-11 19:01:15 +0530

    Displays linked many 2 many tbl by left_outer_join

--- app/controllers/instructors_controller.rb
+++ app/controllers/instructors_controller.rb
@@ -6,6 +6,17 @@ class InstructorsController < ApplicationController
     def inassign
         @course = Course.find(params[:id])
         @groups = @course.groups.all
-        @users = @groups.joins(:users)
+        #@users = @groups.joins("LEFT OUTER JOIN group_users ON groups.id = group_users.group_id LEFT OUTER JOIN users ON group_users.user_id = users.id")
+        #sql = "SELECT users.fname, users.lname, groups.groupname
+        #    FROM users
+        #LEFT OUTER JOIN group_users
+        #    ON users.id = group_users.user_id
+        #LEFT OUTER JOIN groups
+        #    ON group_users.group_id = groups.id"
+        #@users = ActiveRecord::Base.connection.exec_query(sql)
+        @users = User.all
+        @userselected = User.select('users.fname','users.lname','groups.groupname').
+        left_outer_joins(:group_users).
+        left_outer_joins(:groups).all
     end
 end

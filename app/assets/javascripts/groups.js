03a3fc72724affbed28429d45c78835c32d46e78
Terone Madhanayaka <34730459+bterone@users.noreply.github.com>
2019-03-12 14:45:10 +0530

    push 4 implementing devise & pundit (AJAX needed)

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,5 +1,8 @@
 document.addEventListener("turbolinks:load", function() {
     $(".sortable").sortable({
-        connectWith: ".sortable"
+        connectWith: ".sortable",
+        update: function ( event, ui ) {
+            console.log('UPDATE')
+        }
     });
 });
